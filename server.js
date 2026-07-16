c9aa9c59b0b2bf42aaa18df15d6df64356357ee5
Rishi Sharma <rdsharma841@.com>
2026-07-16 18:11:19 +0530

    'DevOr

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.patch('/products/:id', (req, res) => {
   Object.assign(product, req.body, { id });
   res.json(product);
 });
+ 
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
